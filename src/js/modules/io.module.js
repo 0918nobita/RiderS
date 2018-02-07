57c88b42df1731374043f26a9552847694c56539
0918nobita <nobita.0918@gmail.com>
2018-02-07 21:07:09 +0900

    Improve readability

--- src/js/modules/io.module.js
+++ src/js/modules/io.module.js
@@ -1,19 +1,46 @@
 // @flow
+/**
+ * @access public
+ * @desc the class in charge of Input/Output
+ */
 export default class IO {
-	body: ?HTMLElement;
-	first: ?HTMLElement;
+	/**
+	 * @access private
+	 * @type {?Node}
+	 * @desc the last added node
+	 */
+	_first : ?Node;
+
+	/**
+	 * @access private
+	 * @type {Node}
+	 * @desc body element
+	 */
+	_body : Node;
+
+	/**
+	 * @access public
+	 * @desc create IO instance
+	 * @throws {Error} throw error when document.body object cannot be found.
+	 */
 	constructor() {
-		this.body = document.body;
+		this._first = null;
+		if (document.body instanceof Node) this._body = document.body;
+		else throw new Error("document.body cannnot be found.");
 	}
+
+	/**
+	 * @desc output passed string
+	 * @param {string} str - the output string
+	 */
 	output(str : string): void {
-		const p : ?HTMLElement = document.createElement("p");
-		if (p instanceof HTMLElement) {
-			p.textContent = str;
-			if (this.body instanceof HTMLElement) {
-				if (this.first instanceof HTMLElement)
-					this.body.insertBefore(p, this.first);
-				else this.first = this.body.appendChild(p);
-			}
-		}
+		const p : ?Node = document.createElement("p");
+		if (p == null) return;
+		
+		p.textContent = str;
+		
+		this._first = (this._first != null) ?
+			this._body.insertBefore(p, this._first) :
+			this._body.appendChild(p);
 	}
 }
