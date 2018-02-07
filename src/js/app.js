379b6fdbb2b962671bc5e9db87da1d4d41c2c2a5
0918nobita <nobita.0918@gmail.com>
2018-02-07 09:44:39 +0900

    端末の傾きを常時取得して出力するようにした

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,20 @@
+// @flow
+const w : ?EventTarget = window;
 const body : ?HTMLElement = document.body;
-const p : ?HTMLElement = document.createElement("p");
-if (p instanceof HTMLElement) {
-	p.textContent = "Hello World!";
-	if (body instanceof HTMLElement) body.appendChild(p);
+const alpha: ?HTMLElement = document.createElement("p");
+const beta: ?HTMLElement = document.createElement("p");
+const gamma: ?HTMLElement = document.createElement("p");
+
+if (body instanceof HTMLElement) {
+	if (alpha instanceof HTMLElement) body.appendChild(alpha);
+	if (beta instanceof HTMLElement) body.appendChild(beta);
+	if (gamma instanceof HTMLElement) body.appendChild(gamma);
 }
+
+if (w instanceof EventTarget) w.addEventListener("deviceorientation", (e: any) => {
+	if (alpha instanceof HTMLElement && beta instanceof HTMLElement && gamma instanceof HTMLElement) {
+		alpha.textContent = "alpha: " + e.alpha;
+		beta.textContent = "beta: " + e.beta;
+		gamma.textContent = "gamma: " + e.gamma;
+	}
+});
