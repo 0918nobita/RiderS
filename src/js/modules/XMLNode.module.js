dd67da6f1662d413e3a5d260dd8d980a0dc68a4a
0918nobita <nobita.0918@gmail.com>
2018-02-08 19:22:32 +0900

    XMLNode クラスに、子要素を表す children フィールドを追加した

--- src/js/modules/XMLNode.module.js
+++ src/js/modules/XMLNode.module.js
@@ -1,4 +1,21 @@
 // @flow
+/**
+ * @access public
+ * @desc XML Node
+ */
 export default class XMLNode {
-	constructor() {}
+	/**
+	 * @access private
+	 * @desc child nodes
+	 */
+	_children: XMLNode[];
+
+	/**
+	 * @access public
+	 * @param {XMLNode[]} [children] - child nodes
+	 */
+	constructor(children?: XMLNode[]) {
+		if (children !== undefined) this._children = children;
+		else this._children = [];
+	}
 }
