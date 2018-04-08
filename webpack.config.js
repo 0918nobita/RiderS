8d4b2d2736c10638f74cc7b26a729ccb9a6dfefa
0918nobita <nobita.0918@gmail.com>
2018-04-08 16:31:14 +0900

    webpack 4 に移行した

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,51 @@
 "use strict";
+const webpack = require('webpack');
 const path = require('path');
-const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const flowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
 
-module.exports = env => {
-	let config = {
-		// エントリーポイントの設定
-		entry: './src/js/app.js',
-		// 出力の設定
-		output: {
-			// 出力するファイル名
-			filename: 'bundle.js',
-			// 出力先の絶対パス
-			path: path.join(__dirname, 'public/js')
-		},
-		// ローダーの設定
-		module: {
-			rules: [{
-				// ローダーの処理対象ファイル
-				test: /\.js$/,
-				// 除外するディレクトリ
-				exclude: /node_modules/,
-				// 利用するローダー
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: [['env', {
-							targets: {
-								browsers: [
-									'android >= 2',
-									'ios >= 7',
-									'ie >= 10',
-									'chrome >= 50'
-								]
-							}
-						}]],
-						plugins: ['babel-plugin-transform-flow-strip-types']
-					}
-				}]
-			},{
-				// babel-loader よりも優先する
-				enforce: 'pre',
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'eslint-loader'
+module.exports = {
+	// エントリーポイントの設定
+	entry: './src/js/app.js',
+	// 出力の設定
+	output: {
+		// 出力するファイル名
+		filename: 'bundle.js',
+		// 出力先の絶対パス
+		path: path.join(__dirname, 'public/js')
+	},
+	// ローダーの設定
+	module: {
+		rules: [{
+			enforce: 'pre',
+			test: /\.js$/,
+			loader: 'eslint-loader',
+			exclude: /node_modules/,
+			options: {
+				emitWarning: true
+			}
+		},{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					presets: [['env', {
+						targets: {
+							browsers: [
+								'android >= 2',
+								'ios >= 7',
+								'ie >= 10',
+								'chrome >= 50'
+							]
+						}
+					}]],
+					plugins: ['babel-plugin-transform-flow-strip-types']
+				}
 			}]
-		},
-		plugins: [
-			new flowBabelWebpackPlugin()
-		]
-	};
-	if (env !== void 0 && env.production !== void 0) config.plugins.push(new uglifyJsPlugin({uglifyOptions: {ecma: 6}}));
-	return config;
+		}]
+	},
+	plugins: [
+		new flowBabelWebpackPlugin(),
+		new webpack.LoaderOptionsPlugin({ options: {} }),
+	]
 };
