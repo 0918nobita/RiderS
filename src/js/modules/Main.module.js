ef3205c27a16c9bc01179f8b139cc11e6c775aa1
0918nobita <nobita.0918@gmail.com>
2018-04-09 00:19:22 +0900

    位置情報(緯度)を取得するサンプルを追加した

--- src/js/modules/Main.module.js
+++ src/js/modules/Main.module.js
@@ -1,5 +1,5 @@
 // @flow
-import DOMController from "./DOMController.module";
+import DOMController from './DOMController.module';
 
 /**
  * @access public
@@ -21,18 +21,37 @@ export default class Main extends DOMController {
 	 * @desc execute the main procedure.
 	 */
 	main(): void {
-		const alpha: Node = this.createElement("p");
-		const beta: Node = this.createElement("p");
-		const gamma: Node = this.createElement("p");
+		const alpha: Node = this.createElement('p');
+		const beta: Node = this.createElement('p');
+		const gamma: Node = this.createElement('p');
+		const geolocation: Node = this.createElement('p');
 
 		this.body.appendChild(alpha);
 		this.body.appendChild(beta);
 		this.body.appendChild(gamma);
+		this.body.appendChild(geolocation);
 
-		this.window.addEventListener("deviceorientation", (e: any) => {
-			alpha.textContent = "alpha: " + e.alpha;
-			beta.textContent = "beta: " + e.beta;
-			gamma.textContent = "gamma: " + e.gamma;
+		this.window.addEventListener('deviceorientation', (e: any) => {
+			alpha.textContent = 'alpha: ' + e.alpha;
+			beta.textContent = 'beta: ' + e.beta;
+			gamma.textContent = 'gamma: ' + e.gamma;
 		});
+
+		const task: Promise<void> = new Promise(
+			function(resolve: (Promise<void> | void) => void, reject: any => void) {
+				navigator.geolocation.getCurrentPosition(
+					/*success*/ (position: Position) => {
+						geolocation.textContent = 'latitude: ' + position.coords.latitude;
+						resolve();
+					},
+					/*error*/ (error: PositionError) => {
+						geolocation.textContent = 'failed: error code is ' + error.code;
+						reject();
+					}
+				);
+			}
+		);
+
+		task.then();
 	}
 }
