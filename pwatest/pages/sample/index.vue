c3cfa269823febbd6b8dcd3404cb03f2c0728425
0918nobita <nobita.0918@gmail.com>
2018-04-20 19:17:09 +0900

    位置情報を取得するサンプルを追加した

--- pwatest/pages/sample/index.vue
+++ pwatest/pages/sample/index.vue
@@ -7,5 +7,21 @@
 </template>
 
 <script>
-console.log('hello!!');
+export default {}
+
+const task = new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition(
+        /*success*/ (position) => {
+            console.log('latitude: ' + position.coords.latitude);
+            console.log('longitude: ' + position.coords.longitude);
+            resolve();
+        },
+        /*error*/ (error) => {
+            console.log('位置情報の取得に失敗しました');
+            reject();
+        }
+    );
+});
+
+task.then();
 </script>
