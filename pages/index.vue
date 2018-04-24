55725fce690d65e8dbd549cf7b138e5672a23444
0918nobita <nobita.0918@gmail.com>
2018-04-25 00:26:07 +0900

    Move files in pwatest folder to project root directory

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <section class="container">
     <div>
+      <p id="alpha"></p>
+      <p id="beta"></p>
+      <p id="gamma"></p>
+      <p id="latitude"></p>
+      <p id="longitude"></p>
     </div>
   </section>
 </template>
