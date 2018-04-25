da1a5f6067bd18e16a838ee62da46e693bf32334
0918nobita <nobita.0918@gmail.com>
2018-04-26 08:00:30 +0900

    Use mouted hook

--- pages/sample/index.vue
+++ pages/sample/index.vue
@@ -12,28 +12,32 @@ import axios from 'axios';
 
 const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
-async function main() {
-    const success = async (position) => {
-        await reverse_geocoding(position.coords.latitude, position.coords.longitude);
+export default {
+    mounted: function() {
+        this.$nextTick(function() {
+            async function main() {
+                const success = async (position) => {
+                    await reverse_geocoding(position.coords.latitude, position.coords.longitude);
+                },
+                error = (e) => {
+                    console.log('位置情報の取得に失敗しました。');
+                };
+
+                navigator.geolocation.getCurrentPosition(success, error);
+
+                async function reverse_geocoding(latitude, longitude) {
+                    await axios.get(GEOCODE_ENDPOINT, {params: {
+                        latlng: latitude + ', ' + longitude,
+                        key: config.key,
+                        language: 'ja',
+                    }}).then(result => {
+                        console.log(result.data.results[0].formatted_address);
+                    });
+                }
+            }
+
+            main();
+        })
     },
-    error = (e) => {
-        console.log('位置情報の取得に失敗しました。');
-    };
-
-    navigator.geolocation.getCurrentPosition(success, error);
-
-    async function reverse_geocoding(latitude, longitude) {
-        await axios.get(GEOCODE_ENDPOINT, {params: {
-            latlng: latitude + ', ' + longitude,
-            key: config.key,
-            language: 'ja',
-        }}).then(result => {
-            console.log(result.data.results[0].formatted_address);
-        });
-    }
-}
-
-main();
-
-export default {};
+};
 </script>
