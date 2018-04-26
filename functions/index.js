aa95611cf6cba621d3f5f250b7ad4901db26a567
0918nobita <nobita.0918@gmail.com>
2018-04-26 09:02:25 +0900

    Firebaseへのデプロイの設定を追加した

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+function handleRequest(req, res) {
+    res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+    return new Promise((resolve, reject) => {
+        nuxt.render(req, res, (promise) => {
+            promise.then(resolve).catch(reject);
+        });
+    });
+}
+
+app.use(handleRequest);
+exports.ssrapp = functions.https.onRequest(app);
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'pwatest',
+    title: 'Rider-S',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
