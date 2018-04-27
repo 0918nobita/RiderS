28c2bc6835f34e2ae41093c5d63f474983c07c05
0918nobita <nobita.0918@gmail.com>
2018-04-27 18:12:50 +0900

    Add TopBar component

---
+++ components/TopBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class='container'>
+        <div id="header">
+        	<p>Rider-S</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="sass">
+#header
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 50px;
+	font-size: 30px;
+	color: white;
+	background-color: #e26a24;
+	z-index: 3;
+</style>
