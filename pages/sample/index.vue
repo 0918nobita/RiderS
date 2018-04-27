81fb93bfde75717b9e85fe82162e8fdf3f3dae37
0918nobita <nobita.0918@gmail.com>
2018-04-27 18:15:07 +0900

    Init sample page

--- pages/sample/index.vue
+++ pages/sample/index.vue
@@ -1,43 +1,9 @@
 <template>
     <section class='container'>
         <div>
-            <h1>サンプルページ</h1>
         </div>
     </section>
 </template>
 
 <script>
-import config from '~/assets/config';
-import axios from 'axios';
-
-const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
-
-export default {
-    mounted: function() {
-        this.$nextTick(function() {
-            async function main() {
-                const success = async (position) => {
-                    await reverse_geocoding(position.coords.latitude, position.coords.longitude);
-                },
-                error = (e) => {
-                    console.log('位置情報の取得に失敗しました。');
-                };
-
-                navigator.geolocation.getCurrentPosition(success, error);
-
-                async function reverse_geocoding(latitude, longitude) {
-                    await axios.get(GEOCODE_ENDPOINT, {params: {
-                        latlng: latitude + ', ' + longitude,
-                        key: config.key,
-                        language: 'ja',
-                    }}).then(result => {
-                        console.log(result.data.results[0].formatted_address);
-                    });
-                }
-            }
-
-            main();
-        })
-    },
-};
 </script>
