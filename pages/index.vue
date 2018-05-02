81c2b29a4a40dd063b0bf23aafab16c88be7d219
0918nobita <nobita.0918@gmail.com>
2018-05-02 18:13:28 +0900

    位置情報、エラー情報をまとめて一つのテキストエリアに出力するようにした

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,10 @@
   <section class="container">
     <div>
       <topBar/>
-      <div id="console">
-        <!-- <p id="alpha"></p>
-        <p id="beta"></p>
-        <p id="gamma"></p> -->
-        <p id="latitude"></p>
-        <p id="longitude"></p>
-        <p id="address"></p>
-        <p>起動回数: {{count}} 回 <!-- &lt;&lt; <button v-on:click="$store.dispatch('counter/updateCounter')">増加</button>--></p>
-      </div>
       <div id="map"></div>
+      <div id="output">
+          <textarea></textarea>
+      </div>
     </div>
   </section>
 </template>
@@ -26,42 +20,29 @@ const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 export default {
     head: {
         script: [
-            { src: 'https://maps.googleapis.com/maps/api/js?key=' + config.key }
+            { src: 'https://maps.googleapis.com/maps/api/js?key=' + config.google }
         ]
     },
     components: {
         TopBar
     },
-    computed: {
-        count () {
-            return this.$store.state.counter.count;
-        }
-    },
     mounted: function() {
         this.$store.dispatch('counter/updateCounter');
 
         this.$nextTick(function() {
-        	const /* alpha = document.getElementById('alpha'),
-        		beta = document.getElementById('beta'),
-        		gamma = document.getElementById('gamma'), */
-        		latitude = document.getElementById('latitude'),
-        		longitude = document.getElementById('longitude'),
-        		address = document.getElementById('address');
-
-        	/* window.addEventListener('deviceorientation', (e) => {
-        		alpha.textContent = 'alpha: ' + e.alpha;
-        		beta.textContent = 'beta: ' + e.beta;
-        		gamma.textContent = 'gamma: ' + e.gamma;
-        	}); */
+        	const output = document.getElementById('output');
+            
+            output.value += '起動回数: ' + this.$store.state.counter.count + ' 回¥n';
 
             async function main() {
                 const success = async (position) => {
-                	latitude.textContent = '緯度: ' + position.coords.latitude;
-                	longitude.textContent = '経度: ' + position.coords.longitude;
+                	output.value += '緯度: ' + position.coords.latitude + '¥n';
+                	output.value += '経度: ' + position.coords.longitude + '¥n';
                     await reverse_geocoding(position.coords.latitude, position.coords.longitude);
                 },
                 error = (e) => {
                     address.textContent = '緯度、経度の取得に失敗しました。';
+                    output.value += '緯度、軽度の取得に失敗しました。¥n';
                 };
 
                 navigator.geolocation.getCurrentPosition(success, error);
@@ -69,13 +50,14 @@ export default {
                 async function reverse_geocoding(latitude, longitude) {
                     await axios.get(GEOCODE_ENDPOINT, {params: {
                         latlng: latitude + ', ' + longitude,
-                        key: config.key,
+                        key: config.google,
                         language: 'ja',
                     }}).then(result => {
-                    	address.textContent = result.data.results[0].formatted_address;
+                    	output.value += result.data.results[0].formatted_address + '¥n';
                         showMap(latitude, longitude);
                     }).catch(() => {
                         address.textContent = '番地、住所の取得に失敗しました。インターネット接続をご確認ください。';
+                        output.value += '番地、住所の取得に失敗しました。インターネット接続をご確認ください。¥n';
                     });
                 }
 
@@ -102,6 +84,20 @@ export default {
                         strokeWeight: 2,
                         map: map
                     });
+                    strava();
+                }
+
+                async function strava() {
+                    await axios.get("https://www.strava.com/api/v3/activities/", {params: {
+                            id: "1484749096",
+                            access_token: config.strava
+                    }}).then(result => {
+                        console.log(result);
+                        output.value += result + '¥n';
+                    }).catch(() => {
+                        console.log("Strava との連携に失敗しました");
+                        output.value += result + '¥n';
+                    });
                 }
             }
 
@@ -111,24 +107,40 @@ export default {
 };
 </script>
 
-<style scoped lang="sass">
-html, body
+<style scoped lang="scss">
+html, body {
     padding: 0;
     margin: 0;
     width: 100vw;
     height: 100vh;
+}
 
-#console
+#console {
     position: absolute;
     top: 50px;
     left: 0;
     z-index: 1;
+}
 
-#map
+#map {
     position: fixed;
     top: 0;
     left: 0;
     width: 100vw;
     height: 100vh;
     z-index: 0;
+}
+
+#output {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100vw;
+    height: 200px;
+    textarea {
+        width: 100%;
+        height: 100%;
+        resize: none;
+    }
+}
 </style>
