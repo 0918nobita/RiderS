a879dae14007b7379300e8339d8393069dd7c5aa
0918nobita <nobita.0918@gmail.com>
2018-05-08 20:07:19 +0900

    トップページを追加した

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <div id="title">Rider-S</div>
+      <div id="start_button">Start</div>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+#start_button {
+  display: block;
+  position: fixed;
+  width: 150px;
+  height: 150px;
+  line-height: 150px;
+  font-size: 20px;
+  text-decoration: none;
+  text-align: center;
+  color: white;
+  background: #008DDD;
+  border-radius: 50%;
+  left: calc(50% - 50px / 2);
+  bottom: 20px;
+}
+</style>
