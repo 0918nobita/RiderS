e9e6dc16da9f08fb6de756ea13dde3871b2738dc
0918nobita <nobita.0918@gmail.com>
2018-05-14 10:29:04 +0900

    「Powered by Strava」とトップページに表示するようにした

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <section class="container">
     <div>
       <div id="title">Rider-S</div>
+      <div id="content">Powered by Strava</div>
       <router-link to="/navigation" id="start_button">Start</router-link>
     </div>
   </section>
