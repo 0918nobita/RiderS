23a9cf9bb27ae213cbc460a56a30c5e5cc6233c5
0918nobita <nobita.0918@gmail.com>
2018-05-15 09:58:04 +0900

    Fix typo

--- pages/strava_oauth.vue
+++ pages/strava_oauth.vue
@@ -10,7 +10,7 @@ import config from '~/assets/config';
 export default {
   mounted() {
     localStorage.setItem('strava_code', this.$route.query.code);
-    location.href = config.url + '/sample';
+    location.href = config.url + '/strava_settings';
   }
 };
 </script>
