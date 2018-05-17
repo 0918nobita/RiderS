5906f3a723a712b889704d94c8ef738c424739c4
0918nobita <nobita.0918@gmail.com>
2018-05-17 16:13:38 +0900

    トップページでStrava連携の設定を行うようにした

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import config from '~/assets/config';
 
 export default {
@@ -26,9 +27,24 @@ export default {
       let message = 'Error';
       if (localStorage.getItem('strava_code') === null) {
         message = 'コードを取得できていません';
-        window.open(config.url + '/strava_settings', 'Strava連携');
+        location.href = 'http://www.strava.com/oauth/authorize?' +
+          'client_id=' + config.strava.client_id +
+          '&redirect_uri=' + config.strava.redirect_uri +
+          '&response_type=code&scope=public';
       } else if (localStorage.getItem('strava_access_token') === null) {
         message = 'アクセストークンを取得できていません';
+        const params = new URLSearchParams();
+              params.append('client_id', config.strava.client_id);
+              params.append('client_secret', config.strava.client_secret);
+              params.append('code', localStorage.getItem('strava_code'));
+        
+        axios.post('https://www.strava.com/oauth/token', params)
+          .then(response => {
+            localStorage.setItem('strava_access_token', response.data.access_token);
+          })
+          .catch(error => {
+            console.log(error);
+          });
       } else {
         message = 'Success';
         this.$data.showButton = true;
--- pages/strava_oauth.vue
+++ pages/strava_oauth.vue
@@ -8,11 +8,9 @@
 import config from '~/assets/config';
 
 export default {
-  mounted() {
-    this.$nextTick(function() {
-      localStorage.setItem('strava_code', this.$route.query.code);
-      location.href = config.url + '/strava_settings';
-    });
+  mounted () {
+    localStorage.setItem('strava_code', this.$route.query.code);
+    location.href = config.url;
   }
 };
 </script>
